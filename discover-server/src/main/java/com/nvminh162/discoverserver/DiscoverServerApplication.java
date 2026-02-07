95b236573f173a07c7730fad5ce82ff01158e9e0
nvminh162 <nvminh162@gmail.com>
2026-02-07 10:07:17 +0700

    Configure Eureka server settings and enable Eureka server in DiscoverServerApplication

--- discover-server/src/main/java/com/nvminh162/discoverserver/DiscoverServerApplication.java
+++ discover-server/src/main/java/com/nvminh162/discoverserver/DiscoverServerApplication.java
@@ -2,8 +2,10 @@ package com.nvminh162.discoverserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoverServerApplication {
 
 	public static void main(String[] args) {
