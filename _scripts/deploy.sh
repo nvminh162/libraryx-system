f006a69eff30baad2741de390ad05e3d62a9e002
Nguyen Van Minh <nvminh162@gmail.com>
2026-02-28 20:51:44 +0700

    test CI CD

---
+++ _scripts/deploy.sh
@@ -0,0 +1,14 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Deploying with branch: $BRANCH"
+
+cd app/libraryx-system/
+git checkout $BRANCH
+git fetch -a
+git pull
+
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml pull discover-server
+docker compose -f docker-compose.yml up -d discover-server
+# clean up unused images and containers
+docker system prune -af
